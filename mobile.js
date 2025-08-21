4e32943aaaec1560698763f7f6dcd60f7c4c1ea2
island <120293238+island4@users.noreply.github.com>
2025-08-21 10:44:35 +0900

    .

--- mobile.js
+++ mobile.js
@@ -1,6 +1,6 @@
 let test = document.querySelector('#deviceTest');
 let ua = window.navigator.userAgent;
-let device = ua.split('(')
+let device = ua.split('(');
 let newContent = document.createTextNode(device[1]);
 test.appendChild(newContent);
 
