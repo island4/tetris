3a16492d5bff61247f3c95db2d577a5054113352
island <120293238+island4@users.noreply.github.com>
2025-08-22 14:50:56 +0900

    .

--- mobile.js
+++ mobile.js
@@ -32,18 +32,26 @@ leftBtn.firstElementChild.setAttribute('src', './img/LeftMove.png');
 leftBtn.onclick = () => moveBlock('left');
 
 let rightBtn = document.querySelectorAll(".control-btn")[7];
+rightBtn.appendChild(document.createElement('img'));
+rightBtn.firstElementChild.setAttribute('src', './img/RightMove.png');
 rightBtn.onclick = () => moveBlock("right");
 
 //let leftRotateBtn = document.querySelectorAll(".control-btn")[0];
 //leftRotateBtn.onclick = () => moveBlock('left');
 
 let rightRotateBtn = document.querySelectorAll(".control-btn")[5];
+rightRotateBtn.appendChild(document.createElement('img'));
+rightRotateBtn.firstElementChild.setAttribute('src', './img/RightRotate.png');
 rightRotateBtn.onclick = () => rotateBlock();
 
 let downBtn = document.querySelectorAll(".control-btn")[6];
+downBtn.appendChild(document.createElement('img'));
+downBtn.firstElementChild.setAttribute('src', './img/SoftDrop.png');
 downBtn.onclick = () => moveBlock('down');
 
 let holdBtn = document.querySelectorAll(".control-btn")[1];
+holdBtn.appendChild(document.createElement('img'));
+holdBtn.firstElementChild.setAttribute('src', './img/Hold.png');
 holdBtn.onclick = () => holdBlock();
 
 //let leftBtn = document.querySelectorAll(".control-btn")[2];
